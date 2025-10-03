8a9b9bd4b14fca745dae5e008bbb5e38f5ff93a5
José Fernando <166651961+limaamorim@users.noreply.github.com>
2025-10-03 20:28:47 -0300

    Criando o arquivo sistema_eventos.py com a classe já definida

---
+++ src/core/sistema_evento.py
@@ -0,0 +1,30 @@
+class SistemaEventos:
+    def __init__(self):
+        self.__eventos = []
+
+    def cadastrar_novo_evento(self, tipo, dados):
+        pass
+
+    def listar_todos_eventos(self):
+        pass
+
+    def buscar_eventos(self, criterio, valor):
+        pass
+
+    def inscrever_participante(self, evento_id, nome, email):
+        pass
+
+    def cancelar_inscricao(self, evento_id, email):
+        pass
+
+    def fazer_check_in(self, evento_id, email):
+        pass
+
+    def gerar_relatorio_inscritos(self, evento_id):
+        pass
+
+    def listar_vagas_disponiveis(self):
+        pass
+
+    def calcular_receita_total(self, evento_id):
+        pass
